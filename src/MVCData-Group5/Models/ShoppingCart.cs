2657e46abfcd9350589b1117538be20ecb3ade37
Martin Johansson <niteniten58@gmail.com>
2017-08-23 12:30:46 +0200

    Added Partial View for single movie entry

---
+++ src/MVCData-Group5/Models/ShoppingCart.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCData_Group5.Models
+{
+    public class ShoppingCart : Dictionary<int, int>
+    {
+    }
+}
