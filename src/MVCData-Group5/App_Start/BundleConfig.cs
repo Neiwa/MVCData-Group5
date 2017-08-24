9b60d836e8b5a43d016bb5d785dedf84ad1343e7
Martin Johansson <niteniten58@gmail.com>
2017-08-24 14:56:05 +0200

    Styled the increase/decrease buttons in order cart

--- src/MVCData-Group5/App_Start/BundleConfig.cs
+++ src/MVCData-Group5/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace MVCData_Group5
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
